69aada15fd6d3520111b396179276533c4d07b6c
rupalijagta <rupalikk21@gmail.com>
2023-06-21 18:10:24 +0000

    all test cases failed

---
+++ src/components/GameOptions/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const OptionImage = styled.img`
+  width: 170 px;
+  height: 170 px;
+`
+export const OptionListItem = styled.li`
+  list-style-type: none;
+  display: flex;
+`
+export const GameOptionButton = styled.button`
+  outline: none;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+`
